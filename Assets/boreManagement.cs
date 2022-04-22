07d2e1c5252a339323726cbe06292c34f941446b
Cameron Russell <cameronrussell@Camerons-14-MacBook-Pro.local>
2022-04-22 16:21:30 +0100

    bore can move to well

--- Assets/boreManagement.cs
+++ Assets/boreManagement.cs
@@ -8,13 +8,18 @@ public class boreManagement : MonoBehaviour
     public bool moveToWell = true;
     public float speed = 4f; // default speed of tunnel bore - change speed in inspector
     //public float endPoint = 1.0f;
+    // float tempX;
+    // float tempY;
 
+    //     tempX = transform.position.x;
+    //     tempY = transform.position.y;
 
     //Vector2 startPosition = new Vector2 (transform.position.x, 0); 
     //Vector2 endPosition = new Vector2 (-79, 0); // where the well is
 
 
-    Vector2 endPosition = new Vector2(-79f, 1.63f);
+    Vector2 endPosition = new Vector2(-50f, 1.63f);
+    // Vector2 tempPosition = new Vector2(tempX, tempY);
     //Vector2 position = gameObject.transform.position;
 
 
@@ -32,10 +37,22 @@ public class boreManagement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        float step = speed * Time.deltaTime;
+        // tempX = transform.position.x;
+        // tempY = transform.position.y;
+        // Debug.Log(tempX);
+        // Debug.Log(tempY);
 
-        transform.position = Vector2.MoveTowards(transform.position, endPosition, step);
+
+
+
+
+        //if(transform.position.x != -60f)
+        //{
+           float step = speed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, endPosition, step);
+       // }
+
 
 
         //if(transform.position.x != endPoint) 
