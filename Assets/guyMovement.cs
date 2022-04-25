bf6fd42a9566d383b826d668455ce848b31d9f83
Cameron Russell <cameronrussell@Camerons-14-MacBook-Pro.local>
2022-04-25 20:44:02 +0100

    custom character movement script

---
+++ Assets/guyMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guyMovement : MonoBehaviour
+{
+
+    AudioSource footsteps;  // footsteps sound effect
+    public float speed; // speed that the character can move
+    public Rigidbody2D playerRigidBody; // the character rigidbody
+    public float jumpHeight; // how high the character can jump
+
+    // Update is called once per frame (check for user input for movement)
+    void Update()
+    {
+        // if the player presses the horizontal input keys, move the character in the correct direction.
+        float horizontal = Input.GetAxis ("Horizontal");
+        transform.Translate(horizontal * Time.deltaTime * speed, 0f, 0f);
+
+        // if horizontal key inputs are detected play the footsteps sound effect
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            GetComponent<AudioSource> ().Play ();
+
+        }
+        // if horizontal key inputs are not detected, do not play the footsteps sound effect
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            GetComponent<AudioSource> ().Pause ();
+        }
+
+        // if the player presses the space bar, make the character jump
+        if(Input.GetButtonDown ("Jump") && playerRigidBody.velocity.y < 1f && playerRigidBody.velocity.y > -1f)
+        {
+            playerRigidBody.AddRelativeForce(new Vector2(0f, jumpHeight));
+        }
+    }
+}
